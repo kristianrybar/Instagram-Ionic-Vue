ec759389eec7ec81efd15bb7079a22c6a27a8576
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2022-07-19 16:46:32 +0200

    Add files via upload

--- src/views/a-home.vue
+++ src/views/a-home.vue
@@ -2,7 +2,9 @@
   <ion-page>
       <a-header/>
       <ion-content>
-
+        <ion-refresher slot="fixed" @ionRefresh="doRefresh($event)">
+           <ion-refresher-content></ion-refresher-content>
+        </ion-refresher>
         <a-stories/>
         <a-homecontent/>
       </ion-content>
@@ -10,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonContent, } from '@ionic/vue';
+import { IonPage, IonContent, IonRefresher, IonRefresherContent } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import AHeader from '../components/a-header.vue'
 import AStories from '../components/a-stories.vue'
@@ -19,10 +21,17 @@ import AHomecontent from '../components/a-homecontent.vue'
 export default defineComponent({
   name: 'HomePage',
   components: {
-    IonPage, IonContent,
-    AHeader, AStories, AHomecontent,
-  },  
-   
+    IonPage, IonContent,AHeader, AStories, AHomecontent, IonRefresher, IonRefresherContent},
+  methods: {
+    doRefresh(event: any) {
+      console.log('Begin async operation');
+      
+      setTimeout(() => {
+        console.log('Async operation has ended');
+        event.target.complete();
+      }, 2000);
+    }
+  }  
 });
 </script>
 
